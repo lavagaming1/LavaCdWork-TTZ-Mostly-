6f586a8a0814b54e59d8c339e2da5d078a9ef482
lavagaming1 <30357171+lavagaming1@users.noreply.github.com>
2022-02-08 23:16:56 +0300

    stuff

--- MapCapsule.asm
+++ MapCapsule.asm
@@ -1,204 +1,47 @@
-
-
-off_23D9A4:     dc.w unk_23D9B6-*       ; DATA XREF: sub_209EFC+3C↑o
-                                        ; ROM:0023D9A6↓o ...
-                dc.w unk_23D9F4-off_23D9A4
-                dc.w unk_23DA32-off_23D9A4
-                dc.w unk_23DA52-off_23D9A4
-                dc.w unk_23DA58-off_23D9A4
-                dc.w unk_23DA5E-off_23D9A4
-                dc.w unk_23DA64-off_23D9A4
-                dc.w unk_23DA6A-off_23D9A4
-                dc.w unk_23DA70-off_23D9A4
-unk_23D9B6:     dc.b  $C                ; DATA XREF: ROM:off_23D9A4↑o
+off_20D0E6:     dc.w unk_20D0EA-*       ; DATA XREF: ROM:0020C90E↑o
+                                        ; ROM:0020D0E8↓o
+                dc.w unk_20D100-off_20D0E6
+unk_20D0EA:     dc.b   4                ; DATA XREF: ROM:off_20D0E6↑o
+                dc.b $E4
+                dc.b  $F
+                dc.b   0
+                dc.b   0
                 dc.b $E0
-                dc.b   1
-                dc.b   0
-                dc.b   0
-                dc.b $F8
-                dc.b $E0
-                dc.b   1
-                dc.b   8
-                dc.b   0
-                dc.b   0
-                dc.b $F0
-                dc.b   8
-                dc.b   0
-                dc.b   2
-                dc.b $E8
-                dc.b $F0
-                dc.b   8
-                dc.b   0
-                dc.b   5
-                dc.b   0
-                dc.b $F8
-                dc.b  $D
-                dc.b   0
-                dc.b   8
-                dc.b $E0
-                dc.b $F8
-                dc.b  $D
+                dc.b $E4
+                dc.b  $F
                 dc.b   0
                 dc.b $10
                 dc.b   0
-                dc.b   8
-                dc.b  $D
-                dc.b   0
-                dc.b $18
-                dc.b $E0
-                dc.b   8
-                dc.b  $D
-                dc.b   8
-                dc.b $18
-                dc.b   0
-                dc.b $18
-                dc.b  $C
+                dc.b   4
+                dc.b  $E
                 dc.b   0
                 dc.b $20
                 dc.b $E0
-                dc.b $18
-                dc.b  $C
-                dc.b   8
-                dc.b $20
-                dc.b   0
-                dc.b $20
-                dc.b   7
-                dc.b   0
-                dc.b $54 ; T
-                dc.b $F0
-                dc.b $20
-                dc.b   7
-                dc.b   8
-                dc.b $54 ; T
-                dc.b   0
-                dc.b   0
-unk_23D9F4:     dc.b  $C                ; DATA XREF: ROM:0023D9A6↑o
-                dc.b $E0
-                dc.b   1
-                dc.b   0
-                dc.b $24 ; $
-                dc.b $F8
-                dc.b $E0
-                dc.b   1
-                dc.b   8
-                dc.b $24 ; $
-                dc.b   0
-                dc.b $F0
-                dc.b   8
-                dc.b   0
-                dc.b $26 ; &
-                dc.b $E8
-                dc.b $F0
-                dc.b   8
-                dc.b   0
-                dc.b $29 ; )
-                dc.b   0
-                dc.b $F8
-                dc.b  $D
+                dc.b   4
+                dc.b  $E
                 dc.b   0
                 dc.b $2C ; ,
+                dc.b   0
+                dc.b   0
+unk_20D100:     dc.b   4                ; DATA XREF: ROM:0020D0E8↑o
+                dc.b $E4
+                dc.b  $F
+                dc.b   0
+                dc.b $74 ; t
                 dc.b $E0
-                dc.b $F8
-                dc.b  $D
+                dc.b $E4
+                dc.b  $F
                 dc.b   0
-                dc.b $34 ; 4
+                dc.b $84
                 dc.b   0
-                dc.b   8
-                dc.b  $D
+                dc.b   4
+                dc.b  $E
                 dc.b   0
-                dc.b $3C ; <
+                dc.b $94
                 dc.b $E0
-                dc.b   8
-                dc.b  $D
-                dc.b   8
-                dc.b $3C ; <
+                dc.b   4
+                dc.b  $E
                 dc.b   0
-                dc.b $18
-                dc.b  $C
-                dc.b   0
-                dc.b $20
-                dc.b $E0
-                dc.b $18
-                dc.b  $C
-                dc.b   8
-                dc.b $20
-                dc.b   0
-                dc.b $20
-                dc.b   7
-                dc.b   0
-                dc.b $54 ; T
-                dc.b $F0
-                dc.b $20
-                dc.b   7
-                dc.b   8
-                dc.b $54 ; T
-                dc.b   0
-                dc.b   0
-unk_23DA32:     dc.b   6                ; DATA XREF: ROM:0023D9A8↑o
-                dc.b   8
-                dc.b  $D
-                dc.b   0
-                dc.b $44 ; D
-                dc.b $E0
-                dc.b   8
-                dc.b  $D
-                dc.b   0
-                dc.b $4C ; L
-                dc.b   0
-                dc.b $18
-                dc.b  $C
-                dc.b   0
-                dc.b $20
-                dc.b $E0
-                dc.b $18
-                dc.b  $C
-                dc.b   8
-                dc.b $20
-                dc.b   0
-                dc.b $20
-                dc.b   7
-                dc.b   0
-                dc.b $54 ; T
-                dc.b $F0
-                dc.b $20
-                dc.b   7
-                dc.b   8
-                dc.b $54 ; T
-                dc.b   0
-                dc.b   0
-unk_23DA52:     dc.b   1                ; DATA XREF: ROM:0023D9AA↑o
-                dc.b $F8
-                dc.b   5
-                dc.b   0
-                dc.b $5C ; \
-                dc.b $F8
-unk_23DA58:     dc.b   1                ; DATA XREF: ROM:0023D9AC↑o
-                dc.b $F8
-                dc.b   5
-                dc.b   0
-                dc.b $60 ; `
-                dc.b $F8
-unk_23DA5E:     dc.b   1                ; DATA XREF: ROM:0023D9AE↑o
-                dc.b $F8
-                dc.b   5
-                dc.b   0
-                dc.b $64 ; d
-                dc.b $F8
-unk_23DA64:     dc.b   1                ; DATA XREF: ROM:0023D9B0↑o
-                dc.b $F8
-                dc.b   5
-                dc.b   8
-                dc.b $64 ; d
-                dc.b $F8
-unk_23DA6A:     dc.b   1                ; DATA XREF: ROM:0023D9B2↑o
-                dc.b $F8
-                dc.b   5
-                dc.b   8
-                dc.b $60 ; `
-                dc.b $F8
-unk_23DA70:     dc.b   1                ; DATA XREF: ROM:0023D9B4↑o
-                dc.b $F8
-                dc.b   5
-                dc.b   8
-                dc.b $5C ; \
-                dc.b $F8
+                dc.b $A0
+                dcb.b 2,0
+                even
